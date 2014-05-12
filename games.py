4b6c96305e3d6a6dd668d674d47a527a60ce21e0
stvnrlly <stevenmreilly+github@gmail.com>
2014-05-11 21:24:08 -0400

    implement oauth for login

--- games.py
+++ games.py
@@ -1,14 +1,15 @@
-import os
+import os, json
 from datetime import datetime
 from sqlite3 import dbapi2 as sqlite3
+from urllib2 import Request, urlopen, URLError
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
 from flask.ext.wtf import Form
 from flask_wtf.csrf import CsrfProtect
+from flask_oauth import OAuth
 from wtforms import BooleanField, TextField, TextAreaField, PasswordField, \
     HiddenField, validators
 from wtforms.ext.dateutil.fields import DateField, DateTimeField
-from wtforms.widgets import TextArea
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,6 +18,12 @@ from sqlalchemy.ext.declarative import declarative_base
 csrf = CsrfProtect()
 app = Flask(__name__)
 csrf.init_app(app)
+oauth = OAuth()
+
+exec(compile(open("creds.py").read(), "creds.py", 'exec'))
+# GOOGLE_CLIENT_ID stored in creds.py
+# GOOGLE_CLIENT_SECRET stored in creds.py
+REDIRECT_URI = '/oauth2callback'
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -24,7 +31,7 @@ app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='default',
 ))
 app.config.from_envvar('GAMES_SETTINGS', silent=True)
 
@@ -35,6 +42,18 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
+
 class Event(Base):
     __tablename__ = 'events'
     id = Column(Integer, primary_key=True)
@@ -42,33 +61,38 @@ class Event(Base):
     time = Column(String, nullable=False)
     location = Column(String, nullable=False)
     host = Column(String, nullable=False)
-    host_email = Column(String)
     title = Column(String, nullable=False)
     description = Column(String)
     minimum = Column(Integer)
     maximum = Column(Integer)
 
-    def __init__(self, date, time, location, host, host_email, title, description, minimum, maximum):
+    def __init__(self, date, time, location, host, title, description, minimum, maximum):
         self.date = date
         self.time = time
         self.location = location
         self.host = host
-        self.host_email = host_email
         self.title = title
         self.description = description
         self.minimum = minimum
         self.maximum = maximum
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True)
+    oauth_token = Column(String)
+
 class Player(Base):
     __tablename__ = 'players'
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
     event_id = Column(Integer, ForeignKey('events.id'), nullable=False)
-    email = Column(String)
+    email_changes = Column(Integer, nullable=False)
 
-    def __init__(self, name, email, event_id):
+    def __init__(self, name, email_changes, event_id):
         self.name = name
-        self.email = email
+        self.email_changes = email_changes
         self.event_id = event_id
 
 def init_db():
@@ -81,15 +105,14 @@ class NewEvent(Form):
     time = TextField('time', [validators.Required()])
     location = TextField('location', [validators.Required()])
     host = TextField('host', [validators.Required()])
-    host_email = TextField('host_email')
     title = TextField('title', [validators.Optional()])
-    description = TextField('description', [validators.Optional()], widget=TextArea())
+    description = TextAreaField('description', [validators.Optional()])
     minimum = TextField('minimum', [validators.Optional()])
     maximum = TextField('maximum', [validators.Optional()])
 
 class NewPlayer(Form):
     name = TextField('name', [validators.Required()])
-    email = HiddenField('email', [validators.Email(), validators.Optional()])
+    email_changes = BooleanField('email_changes', [validators.Optional()])
     event_id = HiddenField('event_id', [validators.Required()])
 
 class RemovePlayer(Form):
@@ -97,26 +120,29 @@ class RemovePlayer(Form):
 
 class EditEvent(Form):
     id = HiddenField('id')
-    date = DateField('date', [validators.Optional()])
-    time = TextField('time', [validators.Optional()])
-    location = TextField('location', [validators.Optional()])
+    date = DateField('date', [validators.Required()])
+    time = TextField('time', [validators.Required()])
+    location = TextField('location', [validators.Required()])
     host = TextField('host', [validators.Required()])
-    host_email = TextField('host_email')
     title = TextField('title', [validators.Optional()])
-    description = TextField('description', [validators.Optional()], widget=TextArea())
+    description = TextAreaField('description', [validators.Optional()])
     minimum = TextField('minimum', [validators.Optional()])
     maximum = TextField('maximum', [validators.Optional()])
 
 # Make sure to close the database properly
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
+@app.after_request
+def after_request(response):
     db_session.remove()
+    return response
 
 # Create views
 
+@app.route('/', methods=['GET','POST'])
 @app.route('/games', methods=['GET','POST'])
 def games():
+    user = session.get('user')
+    user = User.query.filter(User.id == user).first()
     events = []
     for event in Event.query.all():
         date = datetime.strptime(event.date, '%Y-%m-%d')
@@ -135,15 +161,56 @@ def games():
     new_player = NewPlayer(request.form)
     remove_player = RemovePlayer(request.form)
     edit_event = EditEvent(request.form)
-    return render_template('index.html', events=events, players=players, new_event=new_event, \
+    return render_template('index.html', user=user, events=events, players=players, new_event=new_event, \
                             new_player=new_player, remove_player=remove_player, edit_event=edit_event)
+
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('games'))
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+#    session['access_token'] = access_token, ''
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError, e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        user = res.read()
+    info = json.loads(res.read())
+    user = User.query.filter(User.oauth_token == info['id']).first()
+    if user == None:
+        user = User(name = info['name'], email = info['email'], oauth_token = info['id'])
+        db_session.add(user)
+        db_session.commit()
+        db_session.flush()
+    print user
+    session['user'] = user.id
+    return redirect(url_for('games'))
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
 
 @app.route('/add_event', methods=['POST'])
 def add_event():
     new_event = NewEvent(request.form)
     if new_event.validate_on_submit():
         event = Event(new_event.date.data, new_event.time.data, new_event.location.data, \
-                new_event.host.data, new_event.host_email.data, new_event.title.data, \
+                new_event.host.data, new_event.title.data, \
                 new_event.description.data, new_event.minimum.data, new_event.maximum.data)
         db_session.add(event)
         db_session.commit()
@@ -157,7 +224,7 @@ def add_event():
 def add_player():
     new_player = NewPlayer(request.form)
     if new_player.validate_on_submit():
-        player = Player(new_player.name.data, new_player.email.data, new_player.event_id.data)
+        player = Player(new_player.name.data, new_player.email_changes.data, new_player.event_id.data)
         db_session.add(player)
         db_session.commit()
         db_session.flush()
@@ -206,7 +273,6 @@ def edit_event():
         event.time = edit_event.time.data
         event.location = edit_event.location.data
         event.host = edit_event.host.data
-#       event.host_email = edit_event.host_email.data
         event.title = edit_event.title.data
         event.description = edit_event.description.data
         event.minimum = edit_event.minimum.data
